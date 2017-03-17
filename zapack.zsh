ee55a1cb17df8d0d7d17afccb4dbcba303110888
aiya000 <aiya000.develop@gmail.com>
2017-03-17 15:30:15 +0900

    Add debug variables loading

--- zapack.zsh
+++ zapack.zsh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 source $(dirname $0)/zapack/load_init_script.zsh
 source $(dirname $0)/zapack/git.zsh
+source $(dirname $0)/zapack/debug.zsh
 
 # Add repos to runtime paths
 for repo in $(ls -d "$ZAPACK_REPODIR"/*) ; do
---
+++ zapack/debug.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+# Define variables for debugging
+
+function zpk::check_debug_vars () {
+	local -A opthash
+	zparseopts -D -A opts -- -verbose
+	[ -n "${opts[(i)--verbose]}" ] && echo 1 || echo 0
+}
+
+verbose_is_specified=$(zpk::check_debug_vars $ZAPACK_OPTIONS)
